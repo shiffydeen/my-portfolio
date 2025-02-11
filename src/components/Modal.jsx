dd70adb97d17ceb4c60f36c1182d0b21b2fa5eab
Sheriff Aderombi <ayaderombi@gmail.com>
2025-02-11 11:22:07 +0100

    new project added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -35,12 +35,23 @@ const Modal = ({closeModal, project}) => {
                         ))}
             </div>
             <div className='flex sm:flex-row flex-col flex-wrap gap-3'>
-              <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1">
-                <a href="https://moiveo-app.netlify.app/" target='_blank'>Live Preview</a>
-              </button>
-              <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1"> <BsGithub className='inline mr-2' size={20}/> 
-                <a href="https://github.com/shiffydeen/Movie-App" target='_blank'>Source code</a>
-              </button>
+              <a href={project.demo} target='_blank'>
+                <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1">
+                  Live Preview
+                </button>
+              </a>
+              {/* <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1">
+                <a href={project.demo} target='_blank'>Live Preview</a>
+              </button> */}
+              <a href={project.github} target='_blank'>
+                <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1"> <BsGithub className='inline mr-2' size={20}/> 
+                Source code
+                </button>
+              </a>
+              
+              {/* <button className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] p-2 rounded-md text-white font-light hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out border-2 border-transparent hover:border-[#63e] flex-1"> <BsGithub className='inline mr-2' size={20}/> 
+                <a href={project.github} target='_blank'>Source code</a>
+              </button> */}
             </div>
             
 
--- src/components/portfolioData.js
+++ src/components/portfolioData.js
@@ -5,6 +5,12 @@ import moiveo4 from '../images/moiveo-4.png';
 import moiveo5 from '../images/moiveo-5.png';
 import moiveo6 from '../images/moiveo-6.png';
 
+import udemy1 from '../images/udemy-1.png'
+import udemy2 from '../images/udemy-2.png'
+import udemy3 from '../images/udemy-3.png'
+import udemy4 from '../images/udemy-4.png'
+import udemy5 from '../images/udemy-5.png'
+
 export const projectsData = [
     {
         cover: moiveo6,
@@ -12,15 +18,19 @@ export const projectsData = [
         title: "Moiveo: Your One-Stop for TV Entertainment",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, ducimus voluptatum voluptatem saepe quos qui! Fugiat unde velit dicta.",
         technologies: ["Tailwind", "React", "Axios"],
+        demo: "https://moiveo-app.netlify.app/",
+        github: "https://github.com/shiffydeen/Movie-App"
        
         
     },
     {
-        cover: moiveo1,
-        image: [moiveo1, moiveo2, moiveo3, moiveo4, moiveo5, moiveo6],
-        title: "Udemy Clone",
+        cover: udemy1,
+        image: [udemy2, udemy3, udemy4, udemy5],
+        title: "Udemy Clone - Extra Features",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, ducimus voluptatum voluptatem saepe quos qui! Fugiat unde velit dicta.",
-        technologies: ["HTML", "CSS", "Javascript", "MySQL"]
+        technologies: ["Tailwind", "React"],
+        demo: "https://cdemy-upgrade.netlify.app/",
+        github: "https://github.com/shiffydeen/udemy-clone"
     },
     {
         cover: moiveo1,
@@ -33,13 +43,6 @@ export const projectsData = [
         cover: moiveo1,
         image: [moiveo1, moiveo2, moiveo3, moiveo4, moiveo5, moiveo6],
         title: "Weather App",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, ducimus voluptatum voluptatem saepe quos qui! Fugiat unde velit dicta.",
-        technologies: ["HTML", "CSS", "Javascript", "MySQL"]
-    },
-    {
-        cover: moiveo1,
-        image: [moiveo1, moiveo2, moiveo3, moiveo4, moiveo5, moiveo6],
-        title: "E-commerce",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, ducimus voluptatum voluptatem saepe quos qui! Fugiat unde velit dicta.",
         technologies: ["HTML", "CSS", "Javascript", "MySQL"]
     },

035161e7e79b1debc75cd510d40efa3ec6627834
Sheriff Aderombi <ayaderombi@gmail.com>
2025-03-13 16:02:52 +0100

    project to projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,49 +11,49 @@ const Navbar = () => {
         setIsOpen(!isOpen)
     }
 
-  return (
-    <nav className={`fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly transition-all duration-1000`}>
-      <a href="#" className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100'>A.S</a>
-      <ul className='hidden md:flex gap-10'>
-        <li>
-            <a href="#" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Home</a>
-        </li>
-        <li>
-            <a href="#tech" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Tech</a>
-        </li>
-        <li>
-            <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Project</a>
-        </li>
-        <li>
-            <a href="#contact" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Contact</a>
-        </li>
-        <li>
-            <a href="https://docs.google.com/document/d/1cza1wEDGEyssv8sG6j7zjmGlK3M9Y_-S/edit?usp=sharing&ouid=103448139075573345897&rtpof=true&sd=true" target='_blank' className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Resume</a>
-        </li>
-      </ul>
-      <ul className='hidden md:flex gap-5'>
-            <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100'>
-                <a href="https://www.linkedin.com/in/aderombi-sheriff-388033177/" target='_blank'><BsLinkedin /></a>
-            </li>
-            <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-sky-500 hover:opacity-100'>
-                <a href="https://x.com/sheriff__a" target='_blank'>
-                    <BsTwitterX />
-                </a>
-            </li>
-            <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100'>
-                <a href="https://github.com/shiffydeen" target='_blank'>
-                    <BsGithub />
-                </a>
-            </li>
-      </ul>
+    return (
+        <nav className={`fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly transition-all duration-1000`}>
+            <a href="#" className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-semibold transition-all duration-300 hover:opacity-100'>A.S</a>
+            <ul className='hidden md:flex gap-10'>
+                <li>
+                    <a href="#" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Home</a>
+                </li>
+                <li>
+                    <a href="#tech" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Tech</a>
+                </li>
+                <li>
+                    <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Project</a>
+                </li>
+                <li>
+                    <a href="#contact" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Contact</a>
+                </li>
+                <li>
+                    <a href="https://docs.google.com/document/d/1cza1wEDGEyssv8sG6j7zjmGlK3M9Y_-S/edit?usp=sharing&ouid=103448139075573345897&rtpof=true&sd=true" target='_blank' className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Resume</a>
+                </li>
+            </ul>
+            <ul className='hidden md:flex gap-5'>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100'>
+                    <a href="https://www.linkedin.com/in/aderombi-sheriff-388033177/" target='_blank'><BsLinkedin /></a>
+                </li>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-sky-500 hover:opacity-100'>
+                    <a href="https://x.com/sheriff__a" target='_blank'>
+                        <BsTwitterX />
+                    </a>
+                </li>
+                <li className='cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100'>
+                    <a href="https://github.com/shiffydeen" target='_blank'>
+                        <BsGithub />
+                    </a>
+                </li>
+            </ul>
 
-        {isOpen ? (
-            <BiX className='md:hidden block text-4xl' onClick={menuOpen}/>
-        ) : (
-            <BiMenu className='md:hidden block text-4xl' onClick={menuOpen}/>
-        )}
+            {isOpen ? (
+                <BiX className='md:hidden block text-4xl' onClick={menuOpen} />
+            ) : (
+                <BiMenu className='md:hidden block text-4xl' onClick={menuOpen} />
+            )}
 
-        
+
             <div className={`fixed ${isOpen ? 'right-0' : '-right-full'} top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-1 border-gray-800 bg-black/90 p-12 transition-all duration-700`}>
                 <ul className='flex flex-col gap-8'>
                     <li>
@@ -63,7 +63,7 @@ const Navbar = () => {
                         <a href="#tech" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Tech</a>
                     </li>
                     <li>
-                        <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Project</a>
+                        <a href="#projects" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Projects</a>
                     </li>
                     <li>
                         <a href="#contact" className='cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100'>Contact</a>
@@ -88,10 +88,10 @@ const Navbar = () => {
                     </li>
                 </ul>
             </div>
-        
 
-    </nav>
-  )
+
+        </nav>
+    )
 }
 
 export default Navbar

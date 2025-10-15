5ecdb7bfc5b1c7df22b2d17c0dabd0cd3588885d
shiffydeen <ayaderombi@gmail.com>
2025-10-15 22:40:52 +0100

    adjust

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -48,14 +48,6 @@ const Tech = () => {
             variants={variants}
             initial="hidden"
             whileInView="visible"
-            transition={{duration:0.5}}>
-                <BiLogoRedux className='cursor-pointer text-[80px] text-[#764ABC] transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'/>  
-                <h2 className='text-base font-light text-white md:text-md text-center'>Redux</h2>
-            </motion.div>
-            <motion.div
-            variants={variants}
-            initial="hidden"
-            whileInView="visible"
             transition={{duration:0.5}}>  
                 <BiLogoTypescript className='cursor-pointer text-[80px] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]'/>  
                 <h2 className='text-base font-light text-white md:text-md text-center'>TypeScript</h2>
